3452f94a7f455eea31fdfce4532bdb02153bcf9e
Claus Reinke <CR@tangro.de>
2017-01-30 15:08:51 +0100

    add router to app template

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+import 'bootstrap/dist/css/bootstrap.scss';
+import 'font-awesome/css/font-awesome.css';
+
 import App from './App';
-import './index.css';
+import './index.scss';
+
+import reducers from './store/reducers';
+
+import { requireAuthentication, Login } from './login/authentication';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <Provider store={store}>
+      <Router history={browserHistory}>
+        <Route path='/login' component={Login}/>
+        <Route path='/' component={App} onEnter={requireAuthentication}/>
+      </Router>
+    </Provider>,
+    document.getElementById('root')
 );

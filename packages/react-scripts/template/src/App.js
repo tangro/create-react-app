f26d976cecb4ebbdc6dd0dfea4c8451c7a0f500c
Claus Reinke <CR@tangro.de>
2017-02-02 16:45:29 +0100

    add raw-loader and react-helmet

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Helmet from 'react-helmet';
+
 import './App.scss';
+import PACKAGE from '../package.json';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Helmet title={PACKAGE.name + ' - ' + PACKAGE.version}/>
         <div className="App-header">
           <h2>Welcome to React</h2>
         </div>
